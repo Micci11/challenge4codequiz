d9d9a33a6ef46e2aad31f7f3f08d552e31382cd6
Micci11 <miccibenner@gmail.com>
2023-03-30 21:41:21 -0400

    update

--- script.js
+++ script.js
@@ -1,109 +1,156 @@
 // Questions
-const questions = [
-    {
-      question: "What does HTML stand for?",
-      answers: [
-        "Hyper Text Markup Language",
-        "Hyperlinks and Text Markup Language",
-        "Home Tool Markup Language",
-        "None of the above"
-      ],
-      correctAnswer: "Hyper Text Markup Language"
-    },
-    {
-      question: "What does CSS stand for?",
-      answers: [
-        "Creative Style Sheets",
-        "Cascading Style Sheets",
-        "Computer Style Sheets",
-        "Colorful Style Sheets"
-      ],
-      correctAnswer: "Cascading Style Sheets"
-    },
-    // Add more questions here
-  ];
+var questions = [
+  {
+    question: "Commonly used data types DO Not Include:",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts",
+  },
+  {
+    question: "The condition in an if / else statement is enclosed with ___",
+    answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    correctAnswer: "parenthesis",
+  },
+  {
+    question: "Arrays in Javascript can be used to store _____",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "All of the above",
+  },
+  {
+    question: "String values must be enclosed within ___ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    correctAnswer: "quotes",
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    correctAnswer: "console.log",
+  }
 
-  const startButton = document.getElementById("start-button");
+  // Add more questions here
+];
 
-startButton.addEventListener("click", function() {
+//
+
+var startButton = document.getElementById("start-button");
+let timeLeft = 60;
+let currentQuestionIndex = 0;
+
+startButton.addEventListener("click", function () {
   showQuestion(0);
+  startTimer();
 });
-  
-  // Timer
-  let timeLeft = 60;
-  const timerEl = document.getElementById("timer");
-  
-  const timer = setInterval(function() {
-    if (timeLeft > 0) {
-      timeLeft--;
-      timerEl.textContent = timeLeft.toString();
-    } else {
+
+// Timer
+function startTimer() {
+  var timerEl = document.getElementById("timer");
+
+  var timer = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft.toString();
+
+    if (timeLeft <= 0 || currentQuestionIndex >= questions.length) {
       clearInterval(timer);
       endQuiz();
     }
   }, 1000);
-  
-  // Event Listener
-  const answerButtons = document.querySelectorAll(".answer-button");
-  let currentQuestionIndex = 0;
-  
-  answerButtons.forEach(function(button) {
-    button.addEventListener("click", function(event) {
-      const selectedAnswer = event.target.dataset.answer;
-      const isCorrect = event.target.dataset.correct === "true";
-  
-      if (isCorrect) {
-        // Increment the current question index and show the next question
-        currentQuestionIndex++;
-        showQuestion(currentQuestionIndex);
-      } else {
-        // Decrement the timer and show an incorrect message
-        timeLeft -= 10;
-        // Show the same question again
-        showQuestion(currentQuestionIndex);
-      }
-  
-      if (currentQuestionIndex === questions.length) {
-        endQuiz();
-      }
-    });
+}
+// Event Listener
+// var answerButtons = document.querySelectorAll(".answer-button");
+
+// answerButtons.forEach(function(button) {
+//   button.addEventListener("click", function(event) {
+//     var selectedAnswer = event.target.dataset.answer;
+//     var isCorrect = event.target.dataset.correct === "true";
+
+//     if (isCorrect) {
+//       // Increment the current question index and show the next question
+//       currentQuestionIndex++;
+//       showQuestion(currentQuestionIndex);
+//     } else {
+//       // Decrement the timer and show an incorrect message
+//       timeLeft -= 10;
+//       // Show the same question again
+//       showQuestion(currentQuestionIndex);
+//     }
+
+//     if (currentQuestionIndex === questions.length) {
+//       endQuiz();
+//     }
+//   });
+// });
+
+// Show Question
+function showQuestion(questionIndex) {
+  var questionScreen = document.getElementById("question-screen");
+  var questionText = document.getElementById("question");
+  var answerList = document.getElementById("answers");
+
+  var question = questions[questionIndex];
+  questionText.textContent = question.question;
+
+  answerList.innerHTML = "";
+  question.answers.forEach(function (answer) {
+    var answerItem = document.createElement("li");
+    var answerButton = document.createElement("button");
+    answerButton.textContent = answer;
+    answerButton.dataset.answer = answer;
+    answerButton.dataset.correct = answer === question.correctAnswer;
+    answerButton.classList.add("answer-button");
+    answerButton.addEventListener("click", checkAnswer);
+    answerItem.appendChild(answerButton);
+    answerList.appendChild(answerItem);
   });
-  
-  // Show Question
-  function showQuestion(questionIndex) {
-    const questionScreen = document.getElementById("question-screen");
-    const questionText = document.getElementById("question");
-    const answerList = document.getElementById("answers");
-  
-    const question = questions[questionIndex];
-    questionText.textContent = question.question;
-  
-    answerList.innerHTML = "";
-    question.answers.forEach(function(answer) {
-      const answerItem = document.createElement("li");
-      const answerButton = document.createElement("button");
-      answerButton.textContent = answer;
-      answerButton.dataset.answer = answer;
-      answerButton.dataset.correct = answer === question.correctAnswer;
-      answerButton.classList.add("answer-button");
-      answerItem.appendChild(answerButton);
-      answerList.appendChild(answerItem);
-    });
-  
-    // Fix syntax error by closing function call with a closing parenthesis
-    questionScreen.classList.remove("hide");
+
+  //
+  questionScreen.classList.remove("hide");
+}
+var highScore = 0;
+
+function checkAnswer(e) {
+  console.log(e.target.textContent);
+  //once clicked
+  //  if whatever is clicked is equal to the correctAnswer for this question
+  //    then I would increase score
+  //  else if it's incorrect
+  //    I want to decrease the time that the user has left
+  //
+  //  I want to increase to the next question after checking correct/incorrect
+
+  // highScore = 0;
+
+  if (e.target.textContent == questions[currentQuestionIndex].correctAnswer) {
+    console.log("correct");
+    highScore = highScore + 10;
+  } else {
+    console.log("incorrect");
+    timeLeft -= 7
   }
-  
-  // Define endQuiz function
-  function endQuiz() {
-    const endScreen = document.getElementById("end-screen");
-    const score = document.getElementById("score");
-    const initialsInput = document.getElementById("initials");
-  
-    clearInterval(timer);
-  
-    questionScreen.classList.add("hide");
-    endScreen.classList.remove("hide");
-    score.textContent = timeLeft.toString();
+  console.log(highScore);
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion(currentQuestionIndex);
+  } else {
+    endQuiz();
   }
-  
+}
+
+// Defining endQuiz function
+function endQuiz() {
+  console.log("string" + highScore);
+  var endScreen = document.getElementById("end-screen");
+  var questionScreen = document.getElementById("question-screen");
+  var score = document.getElementById("score");
+  var initialsInput = document.getElementById("initials");
+
+  clearInterval(timer);
+
+  questionScreen.classList.add("hide");
+  endScreen.classList.remove("hide");
+  score.textContent = timeLeft.toString();
+}
+function viewHS() {
+  var highScoreScreen = document.getElementById("high-score");
+  var endScreen = document.getElementById("end-screen");
+  endScreen.classList.add("hide");
+  highScoreScreen.classList.remove("hide");
+}
